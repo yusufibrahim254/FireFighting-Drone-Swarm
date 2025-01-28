96e286f4b8bc96f7cbfc449adc6ed5a2f7b2d2ef
yusufibrahim254 <yusufmostafa0@gmail.com>
2025-01-28 18:50:13 -0500

    My changes for drone, I guessed a lot for this I will adjust it once everyone else has a chance to add their changes to see if I need some of the methods i have

---
+++ src/main/java/org/example/Drone.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Drone implements Runnable{
+    private final Scheduler scheduler;
+
+    public Drone(Scheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    @Override
+    public void run() {
+        while (!scheduler.isStopped()) {
+            Event event = scheduler.getEvent();
+            if (event != null) {
+                processEvent(event);
+                try {
+                    Thread.sleep(700);
+                } catch (InterruptedException e) {
+                    System.out.println("Drone was interrupted while processing an event.");
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+        }
+    }
+
+    private void processEvent(Event event) {
+        System.out.println("Drone processing event: " + event);
+        //I need to wait to see what is going on to see if i need this
+    }
+}
