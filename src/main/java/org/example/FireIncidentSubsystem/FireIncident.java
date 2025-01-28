41b10a74257fea0b8a3b9a50fdb726164d56a85e
Mahad Ahmed <mahad.ahmed613@gmail.com>
2025-01-27 19:10:07 -0500

    stub of the fire incident class

---
+++ src/main/java/org/example/FireIncidentSubsystem/FireIncident.java
@@ -0,0 +1,17 @@
+package org.example.FireIncidentSubsystem;
+
+public class FireIncident {
+
+    public FireIncident(){
+
+    }
+
+    public void readEvents(){
+        // get event from input files
+        // take zone id and read another input file with the zones
+    }
+
+    public void readZone(int zoneId){
+        // using the zone id, find the zone info
+    }
+}
