9ab8281e1ff9d48295e7050d9e8570ad2319c2ce
yusufibrahim254 <yusufmostafa0@gmail.com>
2025-01-28 19:36:25 -0500

    added a couple of variables and methods we might need in the future

--- src/main/java/org/example/Drone.java
+++ src/main/java/org/example/Drone.java
@@ -4,9 +4,15 @@ import org.example.FireIncidentSubsystem.Event;
 
 public class Drone implements Runnable{
     private final Scheduler scheduler;
+    private double agentCapacity;
+    private final double maxAgentCapacity;
+    private boolean bayDoorOpen;
 
-    public Drone(Scheduler scheduler) {
+    public Drone(Scheduler scheduler, double initialCapacity) {
         this.scheduler = scheduler;
+        this.agentCapacity = initialCapacity;
+        this.maxAgentCapacity = initialCapacity;
+        this.bayDoorOpen = false;
     }
 
     @Override
@@ -29,5 +35,15 @@ public class Drone implements Runnable{
     private void processEvent(Event event) {
         System.out.println("Drone processing event: " + event);
         //I need to wait to see what is going on to see if i need this
+    }
+
+    private void openBayDoors() {
+        bayDoorOpen = true;
+        System.out.println("Bay doors opened.");
+    }
+
+    private void closeBayDoors() {
+        bayDoorOpen = false;
+        System.out.println("Bay doors closed.");
     }
 }
