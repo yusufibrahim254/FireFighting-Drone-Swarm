86555b1f58981e21d00511a7e23dd9dd38000fa6
Mahad Ahmed <mahad.ahmed613@gmail.com>
2025-01-29 19:19:14 -0500

    basic unit tests for coordinates class

---
+++ src/test/java/org/example/FireIncidentSubsystem/Helpers/CoordinatesTest.java
@@ -0,0 +1,28 @@
+package org.example.FireIncidentSubsystem.Helpers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinatesTest {
+    private int x;
+    private int y;
+    private Coordinates coordinates;
+    @BeforeEach
+    void setUp() {
+        x = 100;
+        y = 200;
+        coordinates = new Coordinates(x, y);
+    }
+
+    @Test
+    void getX() {
+        assertEquals(100, coordinates.getX());
+    }
+
+    @Test
+    void getY() {
+        assertEquals(200, coordinates.getY());
+    }
+}
