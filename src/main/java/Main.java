7ac1d67a4c06c769a8bf04e081be5a4e8cff5c83
Hundey Kuma <91035874+SuperKamiDende@users.noreply.github.com>
2025-01-29 23:35:12 -0500

    Add Main class threads
    Iteration 1
    
    Starts the 3 threads to initiate the project simulation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+
+import org.example.Scheduler;
+import org.example.FireIncidentSubsystem.FireIncident;
+import org.example.DroneSubsystem;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scheduler scheduler = new Scheduler();
+        FireIncident fireIncident = new FireIncident(scheduler);
+        DroneSubsystem droneSubsystem = new DroneSubsystem(scheduler);
+
+        Thread schedulerThread = new Thread(scheduler);
+        Thread fireThread = new Thread(fireIncident);
+        Thread droneThread = new Thread(droneSubsystem);
+
+        schedulerThread.start();
+        fireThread.start();
+        droneThread.start();
+
     }
 }
