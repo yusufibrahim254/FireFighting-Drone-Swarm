d990a455711c3395bb3c9c620f152ae9b29d3b3b
Mahad Ahmed <mahad.ahmed613@gmail.com>
2025-01-29 19:18:58 -0500

    basic unit tests for zone class

---
+++ src/test/java/org/example/FireIncidentSubsystem/Helpers/ZoneTest.java
@@ -0,0 +1,45 @@
+package org.example.FireIncidentSubsystem.Helpers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZoneTest {
+    private Zone zone;
+    private Coordinates start, end;
+
+    @BeforeEach
+    void setUp() {
+        start = new Coordinates(100, 200);
+        end = new Coordinates(400, 500);
+        zone = new Zone(1, start, end);
+    }
+
+    @Test
+    void getZoneId() {
+        assertEquals(1, zone.getZoneId(), "Zone ID should be 1");
+    }
+
+    @Test
+    void getZoneStart() {
+        assertEquals(start, zone.getZoneStart(), "Zone start should be (100, 200)");
+    }
+
+    @Test
+    void getZoneEnd() {
+        assertEquals(end, zone.getZoneEnd(), "Zone end should be (400, 500)");
+    }
+
+    @Test
+    void zoneStartCoords(){
+        assertEquals(start.getX(), zone.getZoneStart().getX(), "Zone start X coords should be 100");
+        assertEquals(start.getY(), zone.getZoneStart().getY(), "Zone start Y coords should be 200");
+    }
+
+    @Test
+    void zoneEndCoords(){
+        assertEquals(end.getX(), zone.getZoneEnd().getX(), "Zone end X coords should be 400");
+        assertEquals(end.getY(), zone.getZoneEnd().getY(), "Zone end Y coords should be 500");
+    }
+}
