2a7053ab442dd30fc3f82fb4044fd7527b39ff38
Mahad Ahmed <mahad.ahmed613@gmail.com>
2025-01-29 19:19:06 -0500

    basic unit tests for severity class

---
+++ src/test/java/org/example/FireIncidentSubsystem/Helpers/SeverityTest.java
@@ -0,0 +1,25 @@
+package org.example.FireIncidentSubsystem.Helpers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeverityTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setWaterAmount() {
+    }
+
+    @Test
+    void getLevel() {
+    }
+
+    @Test
+    void getWaterAmount() {
+    }
+}
