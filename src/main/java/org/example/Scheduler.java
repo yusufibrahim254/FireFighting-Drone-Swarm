e4ad895704028ea5697267b7da31c264645c4555
Hundey Kuma <91035874+SuperKamiDende@users.noreply.github.com>
2025-01-29 23:19:33 -0500

    Add Scheduler class
    Iteration 1
    
    Implements the Scheduler thread, containing a queue with fire incident
    events. Acts as pass through communicator receiving events from fire
    incident report to drone subsystem and back again confirming drone
    subsystem has processed such events

--- src/main/java/org/example/Scheduler.java
+++ src/main/java/org/example/Scheduler.java
@@ -2,17 +2,64 @@ package org.example;
 
 import org.example.FireIncidentSubsystem.Event;
 
-public class Scheduler {
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Scheduler implements Runnable{
     private boolean stop;
-    private Event event;
+    private final Queue<Event> incidentQueue;
 
-
+    public Scheduler() {
+        this.incidentQueue = new LinkedList<>();
+        this.stop = false;
+    }
 
     public boolean isStopped() {
         return stop;
     }
 
-    public Event getEvent() {
-        return event;
+    public synchronized Event getEvent() {
+        return incidentQueue.poll();
+    }
+
+    public synchronized void addIncident(Event event){
+        incidentQueue.add(event);
+    }
+
+    public boolean isEmpty(){
+        return incidentQueue.isEmpty();
+    }
+
+    @Override
+    public void run() {
+
+        while (!stop) {
+            synchronized (this) {
+                while (incidentQueue.isEmpty()) {
+                    try {
+                        wait();
+                        System.out.println("Scheduler: Notifying fire incident events to DroneSubsystem");
+                        System.out.println();
+                        wait();
+                        System.out.println();
+                        System.out.println("Scheduler: Confirmed DroneSubsystem processed FireIncidentSubsystem events");
+                        break;
+                    } catch (InterruptedException e) {
+                        System.out.println("Scheduler interrupted");
+                        Thread.currentThread().interrupt();
+                        break;
+                    }
+                }
+                stopScheduler();
+            }
+        }
+    }
+
+    public void stopScheduler() {
+        System.out.println("Scheduler stopped.");
+        stop = true;
+        synchronized (this) {
+            notifyAll();
+        }
     }
 }
