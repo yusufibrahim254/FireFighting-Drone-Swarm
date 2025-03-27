98d4c69672f897259e84f0679528c8edd6c02893
Mahad Ahmed <mahad.ahmed613@gmail.com>
2025-03-27 13:14:16 -0400

    added new fault event types

--- src/main/java/org/example/FireIncidentSubsystem/Helpers/EventType.java
+++ src/main/java/org/example/FireIncidentSubsystem/Helpers/EventType.java
@@ -1,5 +1,10 @@
 package org.example.FireIncidentSubsystem.Helpers;
 
 public enum EventType {
-    FIRE_DETECTED, DRONE_REQUEST
+    FIRE_DETECTED,
+    DRONE_REQUEST,
+    INVALID_FIRE_EVENT,
+    DRONE_STUCK,
+    NOZZLE_JAMMED,
+    CORRUPTED_MESSAGE
 }
